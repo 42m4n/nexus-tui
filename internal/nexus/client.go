6767c6ac7846e8493af8308a457e3c9229f7e8af
Arman Beykian <arman.beykian@gmail.com>
2026-09-13 13:37:05 +0330

    fix: remove double base64 encoding of bearer token
    Nexus expects raw token in Authorization header, not base64-wrapped.

--- internal/nexus/client.go
+++ internal/nexus/client.go
@@ -3,7 +3,6 @@ package nexus
 import (
 	"bytes"
 	"crypto/tls"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -46,7 +45,7 @@ func New(rawURL string, insecure bool, username, password, token string) (*Clien
 
 func (c *Client) auth(req *http.Request) {
 	if c.token != "" {
-		req.Header.Set("Authorization", "Bearer "+base64.StdEncoding.EncodeToString([]byte(c.token)))
+		req.Header.Set("Authorization", "Bearer "+c.token)
 		return
 	}
 	if c.user != "" {
